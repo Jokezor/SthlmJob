26dd76d4af2f5334522fcf85771d99776b79f718
EC2 Default User <ec2-user@ip-172-31-37-2.eu-central-1.compute.internal>
2017-11-24 12:13:11 +0000

    Added all files for websrv

---
+++ SamplePage.php
@@ -0,0 +1,20 @@
+<html lang="se">
+<body>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+<head>
+<style>
+h1 {
+    color: #59baff;
+    margin-left: 40px;
+    margin-top: 40px;
+} 
+</style>
+<title> Intro</title>
+</head>
+<h1>Intro </h1>
+
+
+
+
+</body>
+</html>
