0634aaa8d49a40f6a0a09c267726b9a4253b6ee2
Frej Sundqvist <frejsundqvist@live.se>
2017-11-25 13:16:28 +0100

    asd

--- SamplePage_3.php
+++ SamplePage_3.php
@@ -1,5 +1,5 @@
-<?php include "../inc/dbinfo.inc"; ini_set('display_startup_errors', 1); 
-ini_set('display_errors', 1); 
+<?php include "../inc/dbinfo.inc"; ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
 error_reporting(-1);
 ?>
 <html>
@@ -8,7 +8,7 @@ error_reporting(-1);
 <?php
 
   /* Connect to MySQL and select the database. */
-  $conn_string = "host=DB_SERVER port=5439 dbname=DB_DATABASE user=DB_USERNAME password=DB_PASSWORD";
+  $conn_string = "host=" . $DB_SERVER . "port=5439 dbname=DB_DATABASE user=DB_USERNAME password=DB_PASSWORD";
   $connection =  pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
  // if($connection == NULL
   $result = pg_query($db_connection, "SELECT lastname FROM employees");
@@ -127,4 +127,3 @@ function TableExists($tableName, $connection, $dbName) {
   return false;
 }
 ?>
-
