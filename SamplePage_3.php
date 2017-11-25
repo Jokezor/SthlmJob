1b343f8b2083f01f2ecebbc89a75df764912b24c
Frej Sundqvist <frejsundqvist@live.se>
2017-11-25 13:19:23 +0100

    asd

--- SamplePage_3.php
+++ SamplePage_3.php
@@ -8,7 +8,7 @@ error_reporting(-1);
 <?php
 
   /* Connect to MySQL and select the database. */
-  $conn_string = "host=" . $DB_SERVER . "port=5439 dbname=DB_DATABASE user=DB_USERNAME password=DB_PASSWORD";
+  $conn_string = "host=" . DB_SERVER . "port=5439 dbname=DB_DATABASE user=DB_USERNAME password=DB_PASSWORD";
   $connection =  pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
  // if($connection == NULL
   $result = pg_query($db_connection, "SELECT lastname FROM employees");
