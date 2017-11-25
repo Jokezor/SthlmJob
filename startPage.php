52b235075d4dd14e795a592e91d4607bf898de35
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-11-25 19:54:33 +0100

    asd

--- startPage.php
+++ startPage.php
@@ -53,11 +53,11 @@ h1 {
 
 <a id="register" class="smooth"></a>
 <div class="page3" id="page3">
+  <div class = "rubrik">
   Registrera Din Profil
+  </div>
   <div class ="Reg">
-
     <?php include "formtable.php"?>
-
   </div>
 
 </div>
