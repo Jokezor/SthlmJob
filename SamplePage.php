67df928b0ffe4377ad214c5bf0390345aac067d8
EC2 Default User <ec2-user@ip-172-31-37-2.eu-central-1.compute.internal>
2017-11-25 12:52:27 +0000

    changed to php

--- SamplePage.php
+++ SamplePage.php
@@ -1,20 +1,71 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://github.com/kswedberg/jquery-smooth-scroll/blob/master/jquery.smooth-scroll.min.js"></script>
+<script>
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+
+    return false;
+});
+</script>
+
 <html lang="se">
-<body>
-	<link rel="stylesheet" type="text/css" href="mystyle.css">
+
 <head>
 <style>
 h1 {
-    color: #59baff;
-    margin-left: 40px;
-    margin-top: 40px;
-} 
+    color: #125186;
+    margin-left: 0%;
+    margin-top: 0%;
+}
 </style>
-<title> Intro</title>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<div class="topnav"  id="myTopnav">
+  <a href="#contact">Kontakt</a>
+  <a href="#register">Registrera</a>
+  <a href="#about" class="smooth">Om Oss</a>
+  <a href="#home" class="smooth">Hem</a>
+
+</div>
+ <h1> Intro </h1>
+<a id="home" class="smooth"></a>
+<div class="page1" id="page1">
+  <div class="introduction">Välkommen till Intro
+	<div class="subintro">Hitta ditt drömjobb</div> </div>
+</div>
+
 </head>
-<h1>Intro </h1>
+<title>Intro</title>
 
 
+<body>
 
+<a id="about" class="smooth"></a>
+<div class="page2" id="page2">
+  <a id="about" class="smooth"></a>
+  <div class ="Om">
+    Om Tjänsten
+  </div>
+
+</div>
+
+<a id="register" class="smooth"></a>
+<div class="page3" id="page3">
+  <div class ="Reg">
+    Registrera Din Profil
+  </div>
+
+</div>
+
+<a id="contact" class="smooth"></a>
+<div class="page4" id="page4">
+  <div class ="Cont">
+    Kontakta Oss På
+  </div>
+
+</div>
 
 </body>
 </html>
