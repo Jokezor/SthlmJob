5c30b6974ef514c03f54fc64df8fd79e331a4024
Frej Sundqvist <frejsundqvist@live.se>
2017-11-26 18:43:41 +0100

    asd

--- formtable.php
+++ formtable.php
@@ -100,7 +100,7 @@ function AddUser($db_connection, $user_name, $user_address, $user_mail, $user_pa
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-funtion printEmptyfieldMessages($user_name, $user_address, $user_mail, $user_password){
+function printEmptyfieldMessages($user_name, $user_address, $user_mail, $user_password){
    if(!strlen($user_name)){
       ?>
       <script>
