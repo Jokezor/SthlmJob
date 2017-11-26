b2bb0ec8d4a897638b185234a943a4cd541e94fc
Frej Sundqvist <frejsundqvist@live.se>
2017-11-26 16:16:23 +0100

    asd

--- formtable.php
+++ formtable.php
@@ -15,12 +15,12 @@ error_reporting(-1);
 
 <?php
  /* If input fields are populated, add a row to the Employees table. */
- if ($_SERVER["REQUEST_METHOD"] == "POST"){
+ //if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $user_name = htmlentities($_POST['Namn']);
     $user_address = htmlentities($_POST['Adress']);
     $user_mail = htmlentities($_POST['Mail']);
     $user_password = htmlentities($_POST['Lösenord']);
- }
+ //}
 
  if (strlen($user_name) && strlen($user_address) && strlen($user_mail) && strlen($user_password)) {
    AddUser($db_connection, $user_name, $user_address, $user_mail, $user_password);
@@ -100,7 +100,7 @@ pg_close($db_connection);
 function AddUser($db_connection, $user_name, $user_address, $user_mail, $user_password){
 
    // Prepare a query for execution
-   $result = pg_prepare($db_connection, "my_query", 'INSERT INTO users (name, address, mail, password) values ($1, $2, $3, $4)');
+   $result = pg_prepare($db_connection, "my_query", 'INSERT INTO users (name, address, email, password) values ($1, $2, $3, $4)');
 
    // Execute the prepared query.  Note that it is not necessary to escape
    // the string "Joe's Widgets" in any way
