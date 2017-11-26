cf29a918cc8803e017447451e7a1fc36ff520550
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-11-26 15:29:44 +0100

    asd

--- formtable.php
+++ formtable.php
@@ -59,7 +59,9 @@ error_reporting(-1);
       echo "<tr>";
       echo "<td>",$query_data[0], "</td>",
            "<td>",$query_data[1], "</td>",
-           "<td>",$query_data[2], "</td>";
+           "<td>",$query_data[2], "</td>",
+           "<td>",$query_data[3], "</td>",
+           "<td>",$query_data[4], "</td>";
       echo "</tr>";
    }
 
