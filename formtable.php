a55c74be9ac850971dff5c1a2e18e344f154f7c5
Frej Sundqvist <frejsundqvist@live.se>
2017-11-26 18:20:41 +0100

    asd

--- formtable.php
+++ formtable.php
@@ -1,7 +1,8 @@
 <?php include "../inc/dbinfo.inc";
 ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set('display_errors', On);
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+ini_set('html_errors', On);
 ?>
 <html>
 <link rel="stylesheet" type="text/css" href="mystyle.css">
