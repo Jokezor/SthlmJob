4d42eb3ca15069b08d3d5f430ae3ed9fc90c1a90
Frej Sundqvist <frejsundqvist@live.se>
2017-11-27 12:23:59 +0100

    asdf

--- formtable.php
+++ formtable.php
@@ -4,37 +4,39 @@ ini_set('display_errors', On);
 error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 ini_set('html_errors', On);
 ?>
+
 <html>
 <link rel="stylesheet" type="text/css" href="mystyle.css">
 <body>
 
 <?php
-
-  /* Connect to MySQL and select the database. */
+  /* Connect to PostGreSQL and select the database. */
   $conn_string = "host=" . DB_SERVER . " port=5439 dbname=" . DB_DATABASE . " user=" . DB_USERNAME . " password=" . DB_PASSWORD;
   $db_connection =  pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
-
 ?>
 
 <?php
  /* If input fields are populated, add a row to the Users table. */
  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    /* If method is post, scroll page to register */
     ?>
     <script type="text/javascript">
     window.location = "#register";
     </script>
     <?php
-    $user_name = htmlentities($_POST['Namn']);
-    $user_address = htmlentities($_POST['Adress']);
+    $user_name = htmlentities($_POST['Name']);
+    $user_address = htmlentities($_POST['Address']);
     $user_mail = htmlentities($_POST['Mail']);
-    $user_password = ($_POST['Lösenord']);
-    $user_password = hash('sha256', $user_password);
+    $user_password = ($_POST['Password']);
+    $user_password2 = ($_POST['Password2']);
+
+    $user_password_hash = hash('sha256', $user_password);
 
 
 
-    if (strlen($user_name) && strlen($user_address) && strlen($user_mail) && strlen($user_password)) {
-      AddUser($db_connection, $user_name, $user_address, $user_mail, $user_password);
-    }
+   if (validEntries($user_name, $user_address, $user_mail, $user_password, $user_password2)) {
+        AddUser($db_connection, $user_name, $user_address, $user_mail, $user_password_hash);
+   }
 }
 ?>
 
@@ -46,19 +48,19 @@ ini_set('html_errors', On);
     </tr>
     <tr>
       <td>
-	<input type="text" name="Namn" maxlength="45" size="30" value="<?php echo $user_name;?>" placeholder="Namn" />
+	<input type="text" name="Name" maxlength="45" size="30" value="<?php echo $user_name;?>" placeholder="Namn" />
       </td>
       <td>
-	<input type="text" name="Adress" maxlength="90" size="35" value="<?php echo $user_address;?>" placeholder="Adress"/>
+	<input type="text" name="Address" maxlength="90" size="35" value="<?php echo $user_address;?>" placeholder="Adress"/>
       </td>
       <td>
   <input type="text" name="Mail" maxlength="90" size="35" value="<?php echo $user_mail;?>" placeholder="Mail Adress"/>
       </td>
       <td>
-  <input type="password" name="Lösenord" maxlength="90" size="35" placeholder="Lösenord"/>
+  <input type="password" name="Password" maxlength="90" size="35" placeholder="Lösenord"/>
       </td>
       <td>
-  <input type="password" name="Lösenord_test" maxlength="90" size="35" placeholder="Upprepa Lösenord"/>
+  <input type="password" name="Password2" maxlength="90" size="35" placeholder="Upprepa Lösenord"/>
       </td>
   </tr>
   <tr>
@@ -94,6 +96,24 @@ function AddUser($db_connection, $user_name, $user_address, $user_mail, $user_pa
    // Execute the prepared query.
    $result = pg_execute($db_connection, "my_query", array($user_name, $user_address, $user_mail, $user_password));
 
+}
+
+function validEntries($name, $address, $mail, $password, $password2){
+   if(strlen($name) && strlen($address) && strlen($mail) && strlen($password) && strlen($password2)){
+      if($password != $password2){
+         return false;
+      }
+      if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+         return false;
+      }
+      if(0){
+         return false;
+      }
+      return true;
+   }
+   else{
+      return false;
+   }
 }
 ?>
 
