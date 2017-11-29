d739d149b8792e53bd7733e012a6458915e54c19
Frej Sundqvist <frejsundqvist@live.se>
2017-11-29 13:14:33 +0100

    asd

--- uploadCV.php
+++ uploadCV.php
@@ -1,5 +1,8 @@
 <?php
 
+echo basename($_FILES["fileToUpload"]["name"]);
+
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -7,7 +10,7 @@ $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
 echo $target_file;
 echo "\n";
-echo $_FILES["fileToUpload"]["tmp_name"];
+echo $_FILES["fileToUpload"]["name"];
 
 
 /* Check file size */
@@ -28,7 +31,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
