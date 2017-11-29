ef70676ab16eafab90af09304c95785f520f10c5
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-11-29 16:28:42 +0100

    asd

--- uploadCV.php
+++ uploadCV.php
@@ -23,7 +23,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-   echo $_FILES["fileToUpload"]["tmp_name"];
+   //echo $_FILES["fileToUpload"]["tmp_name"];
    //echo $target_file;
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
