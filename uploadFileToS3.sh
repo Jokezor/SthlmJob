547a76c64ead6e340b8b14920a7862dc94da7d69
Frej Sundqvist <frejsundqvist@live.se>
2017-11-30 21:45:18 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for file in /var/www/html/uploads/* ; do
-   php talktomeS3.php $file
-   rm $file
+
+   filename=$(basename "$file")
+   echo $filename
+   #php talktomeS3.php $file
+   #rm $file
 done
