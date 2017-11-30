54367fa3b82c2c788580532f5813bd32cdb16f6a
Jokezor <ec2-user@ip-172-31-37-2.eu-central-1.compute.internal>
2017-11-30 20:11:52 +0000

    asd

--- talktomeS3.php
+++ talktomeS3.php
@@ -19,7 +19,7 @@ try {
         'Bucket' => $bucket,
         'Key'    => $keyname,
         'Body'   => 'this is the body!',
-        'ACL'    => 'public'
+        'ACL'    => 'public-read-write'
     ));
 
     // Print the URL to the object.
