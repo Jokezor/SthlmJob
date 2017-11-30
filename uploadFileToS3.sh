3d0dca48ae9ef5e919b3f70a1cd8659b911cb43b
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-01 00:08:13 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
+var i=0
 for file in uploads/*; do
 
-   b=$(basename $file)
+   b={$(basename $file)}
    echo $b
    #echo $filename
    #php talktomeS3.php $file
    #rm $file
+   i=i+1
 done
