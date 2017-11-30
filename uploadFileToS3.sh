b4934c6cfaa08339f6eca40253fd3f97329610f8
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-11-30 22:03:36 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -5,7 +5,7 @@ for file in /uvar/www/html/uploads/* ; do
    xbase=${file##*/}
    xfext=${xbase##*.}
    xpref=${xbase%.*}
-   echo $xpref
+   echo $xbase
    #echo $filename
    #php talktomeS3.php $file
    #rm $file
