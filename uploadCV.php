6c2e42c3a9eb353c1d8fe833240d408e0bfc498b
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-01 19:28:56 +0100

    asd

--- uploadCV.php
+++ uploadCV.php
@@ -2,6 +2,8 @@
 
 $target_dir = "/var/www/html/uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// Need to get email and remove @ and the dot before the ex .com to com
+$user_mail = htmlentities($_POST['Mail']);
 $uploadOk = 1;
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -23,7 +25,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-
+  // Testing with $user_mail instead of $target_file
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
