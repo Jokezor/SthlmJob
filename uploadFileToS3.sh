a910ab8299e83e0075813f6d8a2124a900f44017
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-01 01:30:12 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+regex='^[]0-9a-zA-Z,!^`@{}=().;/~_|[-]+$'
 
 for file in uploads/*; do
 
    b=$(basename "$file")
-   if [ "$b" != "" ]
+   if [ "$b" =~ $regex ]
    then
      php talktomeS3.php "$b"
      echo "$b"
