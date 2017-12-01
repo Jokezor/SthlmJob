7bc83981152cd6a1224d98b5fc2691ae8187138b
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-01 01:21:04 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -4,10 +4,12 @@
 for file in uploads/*; do
 
    b=$(basename "$file")
+   if (b!="*") do
+     php talktomeS3.php "$b"
+     echo "$b"
+     rm -f "$file"
 
-   php talktomeS3.php "$b"
-   echo "$b"
-   rm -f "$file"
+   fi
    ls uploads
 
 done
