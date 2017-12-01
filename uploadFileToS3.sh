15f18bc3919c21486ec762f29d663d8b00d3fee6
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-01 01:58:08 +0100

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-regex='^[]0-9a-zA-Z,!^`@{}=().;/~_|[-]+$'
+
 
 for file in uploads/*; do
 
 
    b=$(basename "$file")
-   ba=""${b//[[:space:]]/}""
 
-
-      #if [[ "$ba" =~ "$regex" ]]
-   if [[ "$b" -eq "*" ]]
+   # If folder is empty $b ="*"
+   if [[ "$b" != * ]]
    then
      ls uploads
      php talktomeS3.php "$b"
      echo "$b"
      rm -f "$file"
-
    fi
 
 done
