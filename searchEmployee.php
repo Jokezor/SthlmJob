73a2d372393de884e06b2fa9a51ecf47945e3cd5
Frej Sundqvist <frejsundqvist@live.se>
2017-12-04 19:48:13 +0100

    new file

---
+++ searchEmployee.php
@@ -0,0 +1,57 @@
+<?php include "../inc/dbinfo.inc";?>
+<?php
+  /* Connect to PostGreSQL and select the database. */
+  $conn_string = "host=" . DB_SERVER . " port=5439 dbname=" . DB_DATABASE . " user=" . DB_USERNAME . " password=" . DB_PASSWORD;
+  $db_connection =  pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
+?>
+
+<?php
+ /* If input fields are populated, add a row to the Users table. */
+ $search_name = ""; $search_mail = "";
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+   $search_name = htmlentities($_POST['Name']);
+   $search_mail = htmlentities($_POST['Mail']);
+
+    // Prepare a query for execution
+   $result = pg_prepare($dbconn, "my_query", 'SELECT name, address, email FROM users WHERE name = $1');
+   if(!$result){
+      exit("query prepare error");
+   }
+   // Execute the prepared query.
+   $result = pg_execute($dbconn, "my_query", $search_name);
+   if(!$result){
+      exit("query execute error");
+   }
+
+   while ($row = pg_fetch_row($result)) {
+      echo "Namn: $row[0]  Adress: $row[1]   Email: $row[2]";
+      echo "<br />\n";
+   }
+}
+   pg_free_result($result);
+?>
+
+
+
+<?php
+/* Closing connection */
+pg_close($db_connection);
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title> Search </title>
+</head>
+<body>
+   <h1> Search Employee </h1>
+   <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+      <input type="text" id="name" name="Name" size="30%" value="<?php echo $search_name;?>" placeholder="Namn" />
+      <input type="email" id="email" name="Mail" size="30%" value="<?php echo $search_mail;?>" placeholder="Email" />
+      <input type="submit" value="Sök" />
+</body>
+</html>
