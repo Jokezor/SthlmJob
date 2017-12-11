26c4a846caeaa4fcd7bfdb311b6d6d29b6ab6bc7
Frej Sundqvist <frejsundqvist@live.se>
2017-12-11 19:38:56 +0100

    asd

--- searchSuggestions.php
+++ searchSuggestions.php
@@ -15,7 +15,7 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED
    if ($q !== "") {
        $q = strtolower($q);
        $len=strlen($q);
-       foreach($a as $name) {
+       foreach($arr as $name) {
            if (stristr($q, substr($name, 0, $len))) {
                if ($hint === "") {
                    $hint = $name;
@@ -29,4 +29,5 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED
    // Output "no suggestion" if no hint was found or output correct values
    echo $hint === "" ? "no suggestion" : $hint;
 }
+
 ?>
