fcda43b51f6a778cacbdb4a802be5c61f1f1c5dd
Frej Sundqvist <frejsundqvist@live.se>
2017-12-15 18:57:51 +0100

    asd

--- searchSuggestions.php
+++ searchSuggestions.php
@@ -37,7 +37,7 @@
    }
    else{
       header("Content-type: text/xml");
-      echo "<?xml version="1.0" encoding="UTF-8"?>";
+      echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
       echo "<MATCHINGWORDS>";
       for($i = 0; $i < count($sugg); $i++){
          echo "<WORD> " . $sugg[$i] . " </WORD>";
