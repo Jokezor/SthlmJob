30124981ecaf6f50669738de569137e4865affb3
Joakim Olofsson <joakimolovsson@hotmail.com>
2017-12-16 21:41:11 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -106,7 +106,7 @@ pg_close($db_connection);
                                 <button class="fluid ui button" type="submit">Submit</button>
                              </div>
                              -->
-                             <div class="field" style="width:11.8033937422%;">
+                             <div class="field" style="width:61.80339/3%;">
                                 <button class="fluid ui button" type="submit">Submit</button>
                              </div>
                            </div>
