4abda39b63c0bb1132cceae3dc0f32d54060dc3b
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-14 11:12:19 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -43,6 +43,7 @@ pg_close($db_connection);
    <link rel="stylesheet" type="text/css" href="searchstyle.css">
    <link rel="stylesheet" type="text/css" href="node_modules/semantic-ui-dropdown/dropdown.min.css">
    <link rel="stylesheet" type="text/css" href="node_modules/semantic-ui/dist/semantic.min.css">
+   <link rel="stylesheet" type="text/css" href="range.css">
    <script src="jquery-3.2.1.min.js"></script>
    <script src="node_modules/semantic-ui/dist/components/transition.min.js"></script>
    <script src="node_modules/semantic-ui-dropdown/dropdown.min.js"></script>
