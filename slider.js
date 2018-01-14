cb3920644b803bcf03d99149ada4f652d276d32c
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-14 12:24:41 +0100

    asd

--- slider.js
+++ slider.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
    range: true,
    min: 0,
    max: 100,
-   start: 5
    values: [ 75, 300 ],
    slide: function( event, ui ) {
      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
