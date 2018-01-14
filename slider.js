b7dc1e97b1b290b7758f4c7bdbbc591c45655f9e
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-14 12:12:20 +0100

    asd

--- slider.js
+++ slider.js
@@ -7,4 +7,5 @@ $(document).ready(function() {
    start: 5
    //onChange: function(val) { myRangeValue = val; } // assigning the callback argument to your variable each time the value changes
  });
+ $('#range').range('set value', 17); // Sets slider with id 'range' to value 17
 });
