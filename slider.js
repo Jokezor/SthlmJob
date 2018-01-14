9d935a6dae92d2ad8baba00c0799a2699e39ae21
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-14 12:13:29 +0100

    asd

--- slider.js
+++ slider.js
@@ -3,7 +3,7 @@ var myRangeValue; // your javascript variable that will store the value of the s
 $(document).ready(function() {
  $('#range').range({
    min: 0,
-   max: 10,
+   max: 100,
    start: 5
    //onChange: function(val) { myRangeValue = val; } // assigning the callback argument to your variable each time the value changes
  });
