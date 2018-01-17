876eb65607a750d3faabaa1c1d1099a31306b384
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-17 20:31:35 +0100

    asd

--- slider.js
+++ slider.js
@@ -11,6 +11,14 @@ $(document).ready(function() {
    }
    //onChange: function(val) { myRangeValue = val; } // assigning the callback argument to your variable each time the value changes
  });
- $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+ $('#range-3').range({
+     min: 0,
+     max: 10,
+     start: 5,
+     onChange: function(value) {
+       $('#display-3').html(value);
+     }
+   });
+
 });
