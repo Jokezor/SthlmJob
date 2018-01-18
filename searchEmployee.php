651bc6bcbbb2207c22436b3b44f23f59c7eae895
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-18 22:15:45 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -224,7 +224,7 @@ pg_close($db_connection);
          </div>
       </div>
    </div>
-   <div class="ui styled accordion" id="Result">
+   <div class="ui styled accordion">
      <div class="title">
        <i class="dropdown icon"></i>
        What is a dog?
