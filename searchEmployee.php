b38a082b0606fbc92980d1063d5e9f999727aec1
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-23 18:49:14 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -226,6 +226,56 @@ pg_close($db_connection);
       </div>
    </div>
 
+   <h1>Toggle Panels</h1>
+<div id="notaccordion">
+  <h3><a href="#">Section 1</a></h3>
+  <div>
+    <p>
+    Mauris mauris  ante, blandit et, ultrices a, suscipit eget, quam. Integer
+    ut neque. Vivamus  nisi metus, molestie vel, gravida in, condimentum sit
+    amet, nunc. Nam a  nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut
+    odio. Curabitur  malesuada. Vestibulum a velit eu ante scelerisque vulputate.
+    </p>
+  </div>
+  <h3><a href="#">Section 2</a></h3>
+  <div>
+    <p>
+    Sed non urna. Donec et ante. Phasellus eu ligula.  Vestibulum sit amet
+    purus. Vivamus hendrerit, dolor at aliquet laoreet,  mauris turpis porttitor
+    velit, faucibus interdum tellus libero ac justo.  Vivamus non quam. In
+    suscipit faucibus urna.
+    </p>
+  </div>
+  <h3><a href="#">Section 3</a></h3>
+  <div>
+    <p>
+    Nam enim risus, molestie et, porta ac, aliquam ac,  risus. Quisque lobortis.
+    Phasellus pellentesque purus in massa. Aenean in pede.  Phasellus ac libero
+    ac tellus pellentesque semper. Sed ac felis. Sed  commodo, magna quis
+    lacinia ornare, quam ante aliquam nisi, eu iaculis leo  purus venenatis dui.
+    </p>
+    <ul>
+      <li>List item one</li>
+      <li>List item two</li>
+      <li>List item  three</li>
+    </ul>
+  </div>
+  <h3><a href="#">Section 4</a></h3>
+  <div>
+    <p>
+    Cras dictum. Pellentesque habitant morbi tristique  senectus et netus
+    et malesuada fames ac turpis egestas. Vestibulum ante  ipsum primis in
+    faucibus orci luctus et ultrices posuere cubilia Curae;  Aenean lacinia
+    mauris vel est.
+    </p>
+    <p>
+    Suspendisse eu nisl. Nullam ut libero. Integer  dignissim consequat lectus.
+    Class aptent taciti sociosqu ad litora torquent per  conubia nostra, per
+    inceptos himenaeos.
+     </p>
+  </div>
+</div>
+
    <div class="ui styled fluid accordion" id="Result">
     <div class="title">
       <i class="dropdown icon"></i>
@@ -254,7 +304,28 @@ pg_close($db_connection);
      Kontakt
    </div>
 
-
+   <script>
+      $.fn.togglepanels = function(){
+      return this.each(function(){
+       $(this).addClass("ui-accordion ui-accordion-icons ui-widget ui-helper-reset")
+      .find("h3")
+       .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
+       .hover(function() { $(this).toggleClass("ui-state-hover"); })
+       .prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
+       .click(function() {
+         $(this)
+           .toggleClass("ui-accordion-header-active ui-state-active ui-state-default ui-corner-bottom")
+           .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e ui-icon-triangle-1-s").end()
+           .next().slideToggle();
+         return false;
+       })
+       .next()
+         .addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom")
+         .hide();
+      });
+      };
+      $("#notaccordion").togglepanels();
+   </script>
 
    <script>
       $(document).ready(function(){
@@ -297,20 +368,13 @@ pg_close($db_connection);
    }
    </script>
 
-   <!--script language='javascript'>
+   <script language='javascript'>
              $(document).ready(function(){
                 $('.ui.accordion').accordion();
              });
-   </script-->
-
-   <script language='text/javascript'>
-       jQuery(document).ready(function(){
-    $('.ui.accordion').click(function() {
-     $(this).next().toggle('slow');
-     return false;
-    }).next().hide();
-    });
    </script>
+
+
 
 </body>
 </html>
