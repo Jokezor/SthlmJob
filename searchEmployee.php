08997ee2efb944b6fe76d869328199e40722a971
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-27 23:10:27 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -239,8 +239,8 @@ pg_close($db_connection);
     </p>
   </div>
   <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" class="employeees">
-  <h3><a href="#">Section 2</a></h3>
-  <div style="width:70%;">
+  <h3 style="width:90%;"><a href="#">Section 2</a></h3>
+  <div style="width:90%;">
     <p>
     Sed non urna. Donec et ante. Phasellus eu ligula.  Vestibulum sit amet
     purus. Vivamus hendrerit, dolor at aliquet laoreet,  mauris turpis porttitor
@@ -248,8 +248,8 @@ pg_close($db_connection);
     suscipit faucibus urna.
     </p>
   </div>
-  <h3><a href="#">Section 3</a></h3>
-  <div style="width:70%;">
+  <h3 style="width:90%;"><a href="#">Section 3</a></h3>
+  <div style="width:90%;">
     <p>
     Nam enim risus, molestie et, porta ac, aliquam ac,  risus. Quisque lobortis.
     Phasellus pellentesque purus in massa. Aenean in pede.  Phasellus ac libero
@@ -262,8 +262,8 @@ pg_close($db_connection);
       <li>List item  three</li>
     </ul>
   </div>
-  <h3><a href="#">Section 4</a></h3>
-  <div style="width:70%;">
+  <h3 style="width:90%;"><a href="#">Section 4</a></h3>
+  <div style="width:90%;">
     <p>
     Cras dictum. Pellentesque habitant morbi tristique  senectus et netus
     et malesuada fames ac turpis egestas. Vestibulum ante  ipsum primis in
