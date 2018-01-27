d0d6b18e524dc175afbe6202dd096c1382958af0
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-27 22:23:12 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -227,12 +227,11 @@ pg_close($db_connection);
    </div>
 <form>
    <h1 style="text-align: center;">Kandidater</h1>
-<div id="notaccordion">
+<div id="notaccordion" class="Res">
   <label for="subscribeNews">Subscribe to newsletter?</label>
   <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter">
-
-  <div class="Res">
-    <h3><a href="#">Section 1</a></h3>
+  <h3><a href="#">Section 1</a></h3>
+  <div>
     <p>
     Mauris mauris  ante, blandit et, ultrices a, suscipit eget, quam. Integer
     ut neque. Vivamus  nisi metus, molestie vel, gravida in, condimentum sit
@@ -240,9 +239,8 @@ pg_close($db_connection);
     odio. Curabitur  malesuada. Vestibulum a velit eu ante scelerisque vulputate.
     </p>
   </div>
-
-  <div class="Res">
-    <h3><a href="#">Section 2</a></h3>
+  <h3><a href="#">Section 2</a></h3>
+  <div>
     <p>
     Sed non urna. Donec et ante. Phasellus eu ligula.  Vestibulum sit amet
     purus. Vivamus hendrerit, dolor at aliquet laoreet,  mauris turpis porttitor
@@ -250,24 +248,22 @@ pg_close($db_connection);
     suscipit faucibus urna.
     </p>
   </div>
-
-  <div class="Res">
-    <h3><a href="#">Section 3</a></h3>
+  <h3><a href="#">Section 3</a></h3>
+  <div>
     <p>
     Nam enim risus, molestie et, porta ac, aliquam ac,  risus. Quisque lobortis.
     Phasellus pellentesque purus in massa. Aenean in pede.  Phasellus ac libero
     ac tellus pellentesque semper. Sed ac felis. Sed  commodo, magna quis
     lacinia ornare, quam ante aliquam nisi, eu iaculis leo  purus venenatis dui.
+    </p>
     <ul>
       <li>List item one</li>
       <li>List item two</li>
       <li>List item  three</li>
     </ul>
-    </p>
   </div>
-
-  <div class="Res">
-    <h3><a href="#">Section 4</a></h3>
+  <h3><a href="#">Section 4</a></h3>
+  <div>
     <p>
     Cras dictum. Pellentesque habitant morbi tristique  senectus et netus
     et malesuada fames ac turpis egestas. Vestibulum ante  ipsum primis in
