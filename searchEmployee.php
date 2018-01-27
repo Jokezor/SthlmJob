3362b1665f1f3a434fa4f618bfd6c1770c362e03
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-01-27 22:22:24 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -258,12 +258,12 @@ pg_close($db_connection);
     Phasellus pellentesque purus in massa. Aenean in pede.  Phasellus ac libero
     ac tellus pellentesque semper. Sed ac felis. Sed  commodo, magna quis
     lacinia ornare, quam ante aliquam nisi, eu iaculis leo  purus venenatis dui.
-    </p>
     <ul>
       <li>List item one</li>
       <li>List item two</li>
       <li>List item  three</li>
     </ul>
+    </p>
   </div>
 
   <div class="Res">
