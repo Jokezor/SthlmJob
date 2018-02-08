9bdf879eb55527366834818ec2ea89989dfd3283
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-02-08 16:37:06 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -371,8 +371,10 @@ pg_close($db_connection);
    <?php
    for($numOfCandidates = 0; $numOfCandidates < 15; $numOfCandidates++){
       echo '
-  <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" class="employees" style="text-align: right; float:right;">
-  <h3 style="width:90%; margin-top:5px"><a href="#">1. &nbsp; Joakim Olofsson / Personlig Assistent / Umeå</a></h3>
+      <div class="both" style="">
+        <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" class="employees" style="text-align: right; float:right;">
+        <h3 style="width:95%; margin-top:5px margin-right:0px !important;"><a href="#">1. &nbsp; Joakim Olofsson / Personlig Assistent / Umeå</a></h3>
+      </div>
   <div style="width:95%; margin-top:5px;">
     <div class="Tryshiftright">
       <p>
