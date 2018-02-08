977d9905938c7e4521622be2e63839dc69aecd4e
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-02-08 16:00:42 +0100

    asd

--- slider.js
+++ slider.js
@@ -17,17 +17,6 @@ $(function() {
 
 
 $(document).ready(function() {
- $("#slider-range").range({
-   range: true,
-   min: 20000,
-   max: 100000,
-   values: [ 75, 300 ],
-   slide: function( event, ui ) {
-     $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-   }
-   //onChange: function(val) { myRangeValue = val; } // assigning the callback argument to your variable each time the value changes
- });
-
  $('#range-3').range({
      min: 20000,
      max: 100000,
