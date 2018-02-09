91ac8a68e8f60e0045891034b68b754d334cb1d6
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-02-09 22:42:13 +0100

    asd

--- slider.js
+++ slider.js
@@ -1,12 +1,13 @@
 var myRangeValue; // your javascript variable that will store the value of the slider
 
+/*
 $(function()) {
-
   var startDate = new Date();
   // Do your operations
   var endDate   = new Date();
   var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
 }
+*/
 
 
 $(function() {
