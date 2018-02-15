a0052b2681395192826b3b60ce00e7ea206c8422
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-02-15 18:33:42 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -108,7 +108,7 @@ cvsummaryResult<?php include "../inc/dbinfo.inc";?>
       $keywords = array(
          $_POST['keywords0'], $_POST['keywords1'],
          $_POST['keywords2'], $_POST['keywords3'],
-         $_POST['keywords4'], $_POST['keywords5'], $_POST['keywords6'])
+         $_POST['keywords4'], $_POST['keywords5'], $_POST['keywords6']
       );
       calculateScore($allCandidates, $keywords);
    }
