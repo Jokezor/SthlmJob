b328563c02c51f54110c9ddec6ab6884674f6496
Frej Sundqvist <frejsundqvist@live.se>
2018-02-15 19:36:49 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -112,7 +112,7 @@
          pg_free_result($personResult);
       }
 
-      $allCandidates[$userid]["langskills"] = "asdd";
+      $allCandidates[$userid]["businessskills"] = "asdd";
       if(!$businessSkillsResult == false){
          while ($row = pg_fetch_row($businessSkillsResult)){
             $userid = $row[0];   $businessSkill = $row[1];
