1bc69551050217df7a523753800b09167332d7ce
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-03-06 18:04:52 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -434,11 +434,10 @@ function calculateScore($allCandidates, $keywords){
                               <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
-                              <li><a href="#">Choice1</a></li>
-                              <li><a href="#">Choice2</a></li>
-                              <li><a href="#">Choice3</a></li>
+                              <li>Choice1</li>
+                              <li>Choice2</li>
+                              <li>Choice3</li>
                               <li class="divider"></li>
-                              <li><a href="#">Choice..</a></li>
                             </ul>
                            </div>
                            <br>
