4eaff11e057d63dafa841f3a2fc8d54974036b88
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-03-06 19:49:53 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -448,9 +448,9 @@ function calculateScore($allCandidates, $keywords){
                             <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
                             <span class="caret"></span></button>
                             <ul class="dropdown-menu">
-                              <li><a href="#">HTML</a></li>
-                              <li><a href="#">CSS</a></li>
-                              <li><a href="#">JavaScript</a></li>
+                              <li>HTML</li>
+                              <li>CSS</li>
+                              <li>JavaScript</li>
                             </ul>
                           </div>
                            <br>
