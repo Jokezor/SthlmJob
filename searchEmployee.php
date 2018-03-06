a882b4beea2b6d2aab855b4db3af870b2ac3ad98
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-03-06 19:52:57 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -220,8 +220,8 @@ function calculateScore($allCandidates, $keywords){
    <script src="jquery-ui.min.js"></script>
    <script src="node_modules/semantic-ui/dist/components/accordion.js"></script>
    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
 </head>
 
 <body>
