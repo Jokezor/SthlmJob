cbd4478d986ddca5d20b355264f68e3f09c93746
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-03-07 20:53:51 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -220,8 +220,6 @@ function calculateScore($allCandidates, $keywords){
    <script src="jquery-ui.min.js"></script>
    <script src="node_modules/semantic-ui/dist/components/accordion.js"></script>
    <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script-->
 </head>
 
 <body>
@@ -430,27 +428,6 @@ function calculateScore($allCandidates, $keywords){
                                 Save as...
                               </div>
                             </div>
-                           </div>
-                           <div class="btn-group" id="myDropdown">
-                            <a class="btn dropdown-toggle" data-toggle="dropdown">
-                              Menu
-                              <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu">
-                              <li>Choice1</li>
-                              <li>Choice2</li>
-                              <li>Choice3</li>
-                              <li class="divider"></li>
-                            </ul>
-                           </div>
-                           <div class="dropdown" id="myDropdowned">
-                            <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-                            <span class="caret"></span></button>
-                            <ul class="dropdown-menu">
-                              <li>HTML</li>
-                              <li>CSS</li>
-                              <li>JavaScript</li>
-                            </ul>
                           </div>
                            <br>
                            <br>
