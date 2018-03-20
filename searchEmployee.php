91b9c6ce0380fd80ddcd68b0ecf64eaccf1af07c
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-03-20 21:50:21 +0100

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -431,8 +431,9 @@ function calculateScore($allCandidates, $keywords){
                             </div>
                           </div>
                         </div>
-                          <div class="ui sub header" style="width:15.4508475%;">Urval</div>
-                            <div class="ui fluid multiple search special selection dropdown" style ="width:15.4508475%;">
+                        <div class="ui sub header" style="width:15.4508475%!important; max-width: 15.4508475%!important;">Urval</div>
+                         <div class="required field" style="width:15.4508475%;">
+                          <div class="ui fluid multiple search special selection dropdown" style ="overflow: visible;">
                             <i class="dropdown icon"></i>
                             <div class="menu">
                              <div class="item" style="width:100%; margin-bottom:5%;">
@@ -453,6 +454,7 @@ function calculateScore($allCandidates, $keywords){
                              </div-->
                            </div>
                          </div>
+                       </div>
                          <div class="ui sub header" style="width:15.4508475%;">Urval</div>
                            <div class="ui fluid multiple search special selection dropdown" style ="width:15.4508475%;">
                            <i class="dropdown icon"></i>
