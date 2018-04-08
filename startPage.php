023a74a961aee471b03c809813ba0e86de947919
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-04-08 11:49:49 +0200

    asd

--- startPage.php
+++ startPage.php
@@ -16,7 +16,7 @@
   <!--a href="#home" class="navigators">Hem</a-->
   <a href="#whyus" class="navigators">Varför Intro?</a>
   <a href="#register" class="navigators">Registrera</a>
-  <a href="#home" class="h1">In<div style="vertical-align: sub; position:absolute;">≡</div>xtro</a>
+  <a href="#home" class="h1">In<div style="vertical-align: baseline; position:absolute;">≡</div>xtro</a>
 </div>
 
 <a id="home" class="smooth"></a>
