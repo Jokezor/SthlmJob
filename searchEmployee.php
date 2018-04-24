7cc3d6f34ebe37c9a8c1d873e60e0812388736cf
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-04-24 21:47:38 +0200

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -471,7 +471,7 @@ function calculateScore($allCandidates, $keywords, $sortingOut){
    // Sortera kandidater här.
    //print_r($sortIncurrent);
    foreach ($sortIncurrent as $cand => $value) {
-     echo $value;
+     echo $value[0];
    }
 
 }
