141bfd720ae0ec23e96f9e84b72280441d6bf637
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-05-03 01:52:59 +0200

    asd

--- formtable.php
+++ formtable.php
@@ -110,7 +110,7 @@ function AddUser($db_connection, $user_name, $user_address, $user_mail, $user_pa
 
    // Execute the prepared query.
    $usid = pg_execute($db_connection, "get_Userid", array($user_mail));
-   echo "usid: " . $usid[0];
+   print_r($usid);
    if(!$usid){
       return false;
    }
