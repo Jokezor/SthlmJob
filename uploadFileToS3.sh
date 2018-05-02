7a7057fb203388fcb05ab4ae75299bfe9ca79889
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-05-02 11:06:52 +0200

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -8,10 +8,13 @@ for file in /var/www/html/uploads/*; do
    # If folder is empty $b ="*"
    if [[ $b != '*' ]]
    then
-     
+
      #ls uploads
-     php /var/www/html/talktomeS3.php "$b"
-     rm -f "$file"
+     $result = (php /var/www/html/talktomeS3.php "$b")
+     if [$result]
+     then
+       rm -f "$file"
+     fi
 
    fi
 
