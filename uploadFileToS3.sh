73d04ec0f536f2fcda3fb420502f03a22351201f
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-05-03 18:25:17 +0200

    asd

--- uploadFileToS3.sh
+++ uploadFileToS3.sh
@@ -10,13 +10,14 @@ for file in /var/www/html/uploads/*; do
    if [[ $b != '*' ]]
    then
 
-     php /var/www/html/talktomeS3.php "$b"
+
      #ls uploads
 
-     #if php /var/www/html/talktomeS3.php "$b"
-     #then
-     rm -f "$file"
-     #fi
+     if php /var/www/html/talktomeS3.php "$b"
+     then
+       sleep(2)
+       rm -f "$file"
+     fi
 
    fi
 
