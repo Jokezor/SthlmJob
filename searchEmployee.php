f0deb6d9ec86f4bf664d6c7d51c1d6d9a3a3bc53
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-05-07 17:49:44 +0200

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -573,8 +573,8 @@ function calculateScore($allCandidates, $keywords, $sortingOut){
    <link rel="stylesheet" type="text/css" href="node_modules/semantic-ui/dist/semantic.min.css">
    <link rel="stylesheet" type="text/css" href="range.css">
    <link rel="stylesheet" type="text/css" href="jquery-ui.min.css">
-   <link rel="stylesheet" href="style.css" />
-   <link data-require="bootstrap-css@*" data-semver="3.3.1" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+   <!--link rel="stylesheet" href="style.css" />
+   <link data-require="bootstrap-css@*" data-semver="3.3.1" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" /-->
 
    <script src="jquery-3.2.1.min.js"></script>
    <script src="node_modules/semantic-ui/dist/components/transition.min.js"></script>
@@ -583,15 +583,15 @@ function calculateScore($allCandidates, $keywords, $sortingOut){
    <script src="slider.js"></script>
    <script src="jquery-ui.min.js"></script>
    <script src="node_modules/semantic-ui/dist/components/accordion.js"></script>
-   <script src="paging.js"></script>
-   <!--script src="node_modules/angular/angular.js"></script-->
+   <!--script src="paging.js"></script>
+   <script src="node_modules/angular/angular.js"></script>
    <script src="app.js"></script>
    <script src="pagination.js"></script>
 
    <script data-require="angular.js@*" data-semver="1.3.15" src="https://code.angularjs.org/1.3.15/angular.js"></script>
    <script data-require="ui-bootstrap@*" data-semver="0.12.1" src="http://angular-ui.github.io/bootstrap/ui-bootstrap-tpls-0.12.1.min.js"></script>
 
-   <script src="script.js"></script>
+   <script src="script.js"></script-->
    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
