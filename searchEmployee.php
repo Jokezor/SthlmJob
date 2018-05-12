4c2b921f013716eae3b23a968daef0decaaabc99
Joakim Olofsson <joakimolovsson@hotmail.com>
2018-05-12 12:59:41 +0200

    asd

--- searchEmployee.php
+++ searchEmployee.php
@@ -655,7 +655,7 @@ function calculateScore($allCandidates, $keywords, $sortingOut){
          <div>
            <div style="height:10%; background-color: #778794;">
             <h1 style="text-align:center; padding-top: 5%; padding-bottom: 2%; background-color: #778794;"> Sök Kandidater </h1>
-            <img src="images/AdobeStock_127154189.svg" width="50%">
+            <img src="images/AdobeStock_127154189.svg" width="20%">
            </div>
             </div>
             <div class="page1" style="margin-top:1%; padding-top: 2%; padding-bottom: 2%;">
